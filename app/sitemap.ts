ca7a34f51ad51ea6008a425ae197d4311c45d3f8
Jeyson Coronado <84329870+jeyson767@users.noreply.github.com>
2026-03-26 10:50:45 -0500

    Update sitemaps.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,10 +2,31 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://lucerito-clean-web-o1qj.vercel.app'
+
     return [
-        { url: baseUrl, lastModified: new Date(), changeFrequency: 'monthly', priority: 1 },
-        { url: `${baseUrl}/inicio`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
-        { url: `${baseUrl}/productos`, lastModified: new Date(), changeFrequency: 'weekly', priority: 1 },
-        { url: `${baseUrl}/contacto`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.5 },
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/nosotros`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/productos`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/contactanos`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
     ]
 }
